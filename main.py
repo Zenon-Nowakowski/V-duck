733a04204b5c1b68f6bb3ef2b24adc4e34914fa3
Zenon-Nowakowski <zenon.nowakowski8@gmail.com>
2024-02-25 15:51:15 -0600

    update readme, testing audio io

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import discord
 from discord.ext import commands
 import random
 from internal import token
+import subprocess
 
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
-
 There are a number of utility commands being showcased here.
 The command prefix is `:V ` (including the white space after :V).
 '''
@@ -17,7 +17,6 @@ intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix=':V ', description=description, intents=intents)
-
 
 @bot.event
 async def on_ready():
@@ -60,6 +59,45 @@ async def joined(ctx, member: discord.Member):
 @commands.has_permissions(administrator=True)
 async def echo(ctx, *, content):
     await ctx.send(content)
+    await ctx.message.delete()
+
+@bot.command()
+async def join(ctx):
+    '''Joins the voice channel of the user who called the command'''
+    voice_channel_requested = ctx.author.voice.channel
+    if voice_channel_requested:
+        await voice_channel_requested.connect()
+    else:
+        await ctx.send("Where are you? (and I'm so sorry!)")
+    await ctx.message.delete()
+
+@bot.command()
+async def leave(ctx):
+    '''Leave a voice channel'''
+    voice_channel_source = ctx.guild.voice_client
+    if voice_channel_source:
+        await voice_channel_source.disconnect()
+    else:
+        await ctx.send("I am not connected to a voice channel ?:/")
+    await ctx.message.delete()
+
+#work in progress
+@bot.command()
+async def play(ctx, video_url):
+    '''Play a song from YouTube'''
+    voice_channel = ctx.author.voice.channel
+    if voice_channel:
+        voice_client = await voice_channel.connect()
+        try:
+            # Open the video using the default media player on the host machine
+            subprocess.Popen(['xdg-open', video_url])
+            await ctx.send(f'Playing {video_url} in the voice channel.')
+        except Exception as e:
+            await ctx.send(f'An error occurred while playing the video: {str(e)}')
+        finally:
+            await voice_client.disconnect()
+    else:
+        await ctx.send("You need to be in a voice channel to use this command.")
     await ctx.message.delete()
 
 bot.run(token)
---
+++ readme.md
@@ -0,0 +1,13 @@
+# :V
+A silly discord bot for personal use. 
+
+# Installation
+1. Clone the repository:
+   ```shell
+   git clone https://github.com/your-username/your-repo.git
+   ```
+## Dependencies 
+The bot requires the following dependencies: 
+```shell 
+pip install discord pynacl
+```
