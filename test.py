ad7691180533d5211fdf032f5335a206e14439a2
Zenon-Nowakowski <zenon.nowakowski8@gmail.com>
2024-02-25 21:59:22 -0600

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import json 
-import random 
-with open("copypasta.json") as jsonFile:
-    mega = json.load(jsonFile)
-    data = mega["copyastas"]
-    pasta = random.choice(data)
-    message = f"# {(pasta)['name']}\n{(pasta)['text']}"
-print(message)
